09b27cf39405250e10b51caa63f8bff2e79a1355
Adam Jeliński <adam.jelinski.stud@pw.edu.pl>
2023-11-22 14:38:41 +0100

    Basic docker images with build and run scripts

---
+++ zad1.1/client.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+docker run -it --network-alias z26_z11_client --hostname z26_z11_client --network z26_network --name z26_z11_client z26_z11_client_image
---
+++ zad1.1/server.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+docker run -it --network-alias z26_z11_server --hostname z26_z11_server --network z26_network --name z26_z11_server z26_z11_server_image
