d005f3a2bf728ba3142c170a07e312e553fc373d
Adam Jeliński <adas.jelinski@gmail.com>
2023-12-13 18:51:09 +0100

    Remove incorrect zad2.3

--- zad2.3/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker build -t z26_z23_server_image ./server
-docker build -t z26_z23_client_image ./client
--- zad2.3/client.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-docker run -it --rm --network-alias z26_z23_client --hostname z26_z23_client --network z26_network --name z26_z23_client z26_z23_client_image
