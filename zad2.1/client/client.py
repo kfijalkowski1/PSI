1b4af8bd387a78ccd5c6ac69fd3977d3877b5bf7
Adam Jeliński <adas.jelinski@gmail.com>
2023-12-13 15:06:28 +0100

    test examples

--- zad2.1/client/client.py
+++ zad2.1/client/client.py
@@ -1,39 +1,22 @@
 ﻿import socket
 import sys
-import io
-import time
 
-host_address = "z26_z21_server"
+HOST = "z26_z21_server"  # The server's hostname or IP address
 
 if len(sys.argv) < 2:
-    print("no port provided, using 8000")
+    print("no port, using 8000")
     port = 8000
 else:
     port = int(sys.argv[1])
 
-with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-    frame_id = 1
-    while True:
-        data = "314159265358979323846264338327950288419716939937510".encode("ascii")
-        data_length = len(data)
-        data_frame = (
-            frame_id.to_bytes(4, "little") + data_length.to_bytes(2, "little") + data
-        )
+print("Will connect to ", HOST, ":", port)
 
-        binary_stream = io.BytesIO()
-        binary_stream.write(data_frame)
-        binary_stream.seek(0)
-        stream_data = binary_stream.read()
-        print("Sending message", repr(data), "of length =", data_length)
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, port))
+    #    s.sendall(b'0123456789')
+    # what happens when we send more data to server accepting only 10B at a time?
+    s.sendall(b"Hello, world!")
+    data = s.recv(1024)
 
-        s.sendto(stream_data, (host_address, port))
-        response = s.recv(1024)
-
-        print(
-            "Message id:",
-            int.from_bytes(response[:4], "little"),
-            "Success" if response[4] == 0 else "Failed",
-        )
-
-        frame_id += 1
-        time.sleep(0.1)
+print("Received", repr(data))
+print("Client finished.")
--- zad2.1/server/server.c
+++ zad2.1/server/server.c
@@ -1,87 +1,75 @@
-﻿#include <stdio.h>
-#include <sys/socket.h>
+﻿#include <arpa/inet.h>
 #include <netdb.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
 
-#define BUFFER_SIZE 500
+#define TRUE 1
+#define BSIZE 1024
 
-#define throwError(msg) { fprintf(stderr, msg); exit(1); }
+#define bailout(s) {perror(s); exit(1); }
+#define notDone() TRUE
 
-int main(int argc, char *argv[]) {
-    int socketFD;
+#define DEF_PORT "8000"
 
+int moreWork(void) {
+    return 1;
+}
+
+int  main(int argc, char **argv) {
+    int sock, msgsock, rval, ListenQueueSize=5;
+    socklen_t length;
+    struct addrinfo *bindto_address;
+    struct addrinfo hints;
     struct sockaddr_in server;
-    struct sockaddr_storage peerAddress;
+    char buf[BSIZE];
 
-    unsigned char inputBuffer[BUFFER_SIZE];
-    unsigned char response[5];
 
-    uint32_t previousMessageId = 0;
+    /* initialize socket type, address and port */
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+    if ( getaddrinfo(0, DEF_PORT, &hints, &bindto_address) != 0 ) 
+        bailout("getting local address");
 
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s port\n", argv[0]);
-        exit(1);
-    }
+    sock = socket(bindto_address->ai_family, bindto_address->ai_socktype, bindto_address->ai_protocol);
+    if (sock == -1) 
+        bailout("opening stream socket");
 
-    if ((socketFD = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-        throwError("socket creation failed\n");
-    }
+    /* dowiaz adres do gniazda */
+    if (bind(sock, bindto_address->ai_addr, bindto_address->ai_addrlen )
+            == -1) 
+        bailout("binding stream socket");
+    freeaddrinfo(bindto_address);
 
-    server.sin_family = AF_INET;
-    server.sin_addr.s_addr = INADDR_ANY;
-    server.sin_port = htons(atoi(argv[1]));
+    /* zacznij przyjmowaæ polaczenia... */
+    listen(sock, ListenQueueSize);
 
-    if (bind(socketFD, (struct sockaddr *) &server, sizeof(server)) < 0) {
-        throwError("port bind failed\n");
-    }
+    do {
+        msgsock = accept(sock,(struct sockaddr *) 0, (socklen_t *) 0);
+        if (msgsock == -1 ) {
+            bailout("accept"); 
+	} else do {
+                memset(buf, 0, sizeof buf);
+                if ((rval = read(msgsock,buf, BSIZE)) == -1)
+                    bailout("reading stream message");
+                if (rval == 0)
+                    printf("%s: Ending connection\n", argv[0]);
+                else
+                    printf("%s: -->%s\n", argv[0], buf);
+            } while (rval != 0);
+        close(msgsock);
+        fflush( stdout );
+    } while( moreWork() );
+    /*
+     * gniazdo sock nie zostanie nigdy zamkniete jawnie,
+     * jednak wszystkie deskryptory zostana zamkniete gdy proces
+     * zostanie zakonczony (np w wyniku wystapienia sygnalu)
+     */
 
-    printf("Waiting for transmision\n");
-    while (1) {
-        socklen_t peerAddressLen = sizeof(peerAddress);
-
-        ssize_t dataLength = recvfrom(socketFD, inputBuffer, BUFFER_SIZE - 1, 0, 
-            (struct sockaddr *) &peerAddress, &peerAddressLen);
-
-        if (dataLength < 0) {
-            fprintf(stderr, "Packet receive failed\n");
-            continue;
-        }
-
-        inputBuffer[dataLength] = '\0';
-
-        printf("Received %zd bytes\n", dataLength);
-
-        if (dataLength < 6) {
-            printf("Message too short, dropping\n");
-            continue;
-        }
-
-        uint32_t messageId = inputBuffer[0] + (inputBuffer[1] << 8) + (inputBuffer[2] << 16) + (inputBuffer[3] << 24);
-        uint32_t messageLength = inputBuffer[4] + (inputBuffer[5] << 8);
-
-        printf("Message id: %u, length: %u\n", messageId, messageLength);
-        printf("Start of payload: %.*s...\n", 10, inputBuffer+6);
-
-        response[0] = inputBuffer[0];
-        response[1] = inputBuffer[1];
-        response[2] = inputBuffer[2];
-        response[3] = inputBuffer[3];
-        response[4] = 0;
-
-        if (messageLength + 6 != dataLength){
-            printf("Incorrect message length\n");
-            response[4] = 1;
-        }
-        if (previousMessageId + 1 != messageId){
-            printf("Incorrect message id\n");
-            response[4] = 1;
-        }
-
-        if (sendto(socketFD, response, sizeof(response), 0, 
-            (struct sockaddr *) &peerAddress, peerAddressLen) < 0) {
-
-            printf("Failed to send response\n");
-        }
-
-        previousMessageId = messageId;
-    }
+    exit(0);
 }
