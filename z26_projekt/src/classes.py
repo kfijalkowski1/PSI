140b33504438a0d9b5ef7fe09a049781e0e2fe4d
Maciej Bienkowski <maciej.bienkowski2.stud@pw.edu.pl>
2023-12-30 15:15:24 +0100

    creating folder scanner and FileState class

---
+++ z26_projekt/src/classes.py
@@ -0,0 +1,18 @@
+﻿import threading
+from datetime import datetime
+
+class FileState:
+    def __init__(self, name, modification_timestamp=None, status='aktualny'):
+        self.name = name
+        self.modification_timestamp = modification_timestamp if modification_timestamp is not None else time.time()
+        self.status = status
+
+    def update(self, modification_timestamp=None, status=None):
+        if modification_timestamp:
+            self.modification_timestamp = modification_timestamp
+        if status:
+            self.status = status
+
+    def __repr__(self):
+        return f"FileState(name='{self.name}', modification_timestamp='{self.modification_timestamp}', status='{self.status}')"
+
---
+++ z26_projekt/src/folder_scanner.py
@@ -0,0 +1,45 @@
+import os
+import time
+from datetime import datetime
+from classes import FileState
+import globals
+
+def scan_folder(folder_path):
+    print(f"Starting folder scan for: {folder_path}")
+
+    while True:
+
+        with globals.folder_state_lock:
+            print("Scanning folder...")
+            current_files = set(os.listdir(folder_path))
+
+
+            for file in current_files:
+                file_path = os.path.join(folder_path, file)
+                modification_timestamp = os.path.getmtime(file_path)
+
+                if file in globals.folder_state:
+                    if globals.folder_state[file].modification_timestamp != modification_timestamp:
+
+                        print(f"file updated: {file}")
+                        globals.folder_state[file].update(modification_timestamp, 'aktualny')
+                else:
+                    print(f"new file detected: {file}")
+                    globals.folder_state[file] = FileState(file, modification_timestamp, 'aktualny')
+
+
+            known_files = set(globals.folder_state.keys())
+            removed_files = known_files - current_files
+            for file in removed_files:
+                print(f"File removed: {file}")
+                del globals.folder_state[file]
+
+        print("current state:")
+        for file, state in globals.folder_state.items():
+            print(f"{file}: {state}")
+
+        print("scan completed waiting for next scan...")
+        time.sleep(30)
+
+if __name__ == "__main__":
+    scan_folder('/home/users/mbienko2/projekt/psi/z26_projekt/src')  # Replace with your folder path
