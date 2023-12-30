6d77dd32f725e3fc7311124d287579e262d2a141
Adam Jeliński <adas.jelinski@gmail.com>
2023-12-30 19:58:17 +0100

    Add packaging, queues and error handling

--- z26_projekt/src/app/classes.py
+++ z26_projekt/src/app/classes.py
@@ -1,10 +1,21 @@
 ﻿import threading
 from datetime import datetime
+import queue
+import time
+import socket
+
+import globals
+import logger
+
 
 class FileState:
-    def __init__(self, name, modification_timestamp=None, status='aktualny'):
+    def __init__(self, name, modification_timestamp=None, status="aktualny"):
         self.name = name
-        self.modification_timestamp = modification_timestamp if modification_timestamp is not None else time.time()
+        self.modification_timestamp = (
+            modification_timestamp
+            if modification_timestamp is not None
+            else time.time()
+        )
         self.status = status
 
     def update(self, modification_timestamp=None, status=None):
@@ -16,3 +27,33 @@ class FileState:
     def __repr__(self):
         return f"FileState(name='{self.name}', modification_timestamp='{self.modification_timestamp}', status='{self.status}')"
 
+
+class ConnectionState:
+    def __init__(self, sock: socket.socket, address, port, client_id):
+        self.socket = sock
+        self.address = address
+        self.port = port
+        self.client_id = client_id
+        self.transmit_queue = queue.Queue()
+
+        self.open = True
+
+    def close(self):
+        if self.open is False:
+            return
+
+        self.open = False
+
+        # TODO: use dedicated class when data_parser is ready
+        self.transmit_queue.put(b"close")
+        self.socket.shutdown(socket.SHUT_RDWR)
+        self.socket.close()
+        logger.success(f"Closed connection with {self.client_id}")
+
+        del globals.CONNECTIONS[self.client_id]
+
+    def __del__(self):
+        self.socket.close()
+
+    def __repr__(self):
+        return f"ConnectionState(client_id='{self.client_id}', address={self.address}, port={self.port})"
--- z26_projekt/src/app/tcp_handler.py
+++ z26_projekt/src/app/tcp_handler.py
@@ -1,57 +1,89 @@
 import socket
-import time
 import struct
 import uuid
 
 import globals
 import logger
 from utils import ExceptThread
+from classes import ConnectionState
 
 
 class Sender(ExceptThread):
-    def __init__(self, sock, address, port, client_id):
-        super().__init__(f"tcp_sender{client_id}")
+    def __init__(self, connection):
+        super().__init__(f"tcp_sender{connection.client_id}")
 
-        self.sock = sock
-        self.address = address
-        self.port = port
-        self.client_id = client_id
+        self.conn = connection
 
     def main(self):
-        s = self.sock
+        s = self.conn.socket
+        queue = self.conn.transmit_queue
         try:
             while True:
-                s.sendall(b"Hello, world!")
+                message = queue.get()
+                # TODO: use dedicated class when data_parser is ready
+                if message == b"close":
+                    logger.debug(f"Sender disconnecting from {self.conn.client_id}")
+                    return
 
-                time.sleep(5)
+                # TODO: handle and serialise message
+
+                prefix = struct.pack("I", len(message))
+                s.sendall(prefix)
+                s.sendall(message)
+                message = None
         except BrokenPipeError:
-            logger.warning(
-                f"Socket closed unexpectedly, sender disconnecting from {self.client_id}"
+            logger.debug(
+                f"Socket closed unexpectedly, sender disconnecting from {self.conn.client_id}"
             )
+            self.conn.close()
 
 
 class Reciever(ExceptThread):
-    def __init__(self, sock, address, port, client_id):
-        super().__init__(f"tcp_reciever{client_id}")
+    def __init__(self, connection):
+        super().__init__(f"tcp_reciever{connection.client_id}")
 
-        self.sock = sock
-        self.address = address
-        self.port = port
-        self.client_id = client_id
+        self.conn = connection
 
     def main(self):
-        s = self.sock
-        while True:
-            data = s.recv(1024)
-            if len(data) == 0:
-                logger.warning(
-                    f"Socket closed unexpectedly, reciever disconnecting from {self.client_id}"
-                )
-                return
+        s = self.conn.socket
+        try:
+            while True:
+                data_len_raw = s.recv(4)
+                if len(data_len_raw) == 0:
+                    self.emergency_close()
+                    return
 
-            logger.info("Recieved " + str(data))
+                (data_len,) = struct.unpack("I", data_len_raw)
 
-            time.sleep(5)
+                data = bytearray()
+                while len(data) < data_len:
+                    tmp = s.recv(data_len - len(data))
+                    if len(tmp) == 0:
+                        self.emergency_close()
+                        return
+                    data.extend(tmp)
+
+                if len(data) < data_len:
+                    logger.warning(
+                        f"Recieved less data than expected! (expected {data_len}, got {len(data)})"
+                    )
+                    self.emergency_close()
+                    return
+
+                logger.info("Recieved " + str(len(data)) + " bytes")
+
+                # TODO: parse and handle message
+
+                data = None
+
+        except ConnectionResetError:
+            self.emergency_close()
+
+    def emergency_close(self):
+        logger.debug(
+            f"Socket closed unexpectedly, reciever disconnecting from {self.conn.client_id}"
+        )
+        self.conn.close()
 
 
 def accept(sock: socket.socket, address, port):
@@ -66,14 +98,11 @@ def accept(sock: socket.socket, address, port):
     (client_id,) = struct.unpack("16s", data)
     client_id = uuid.UUID(bytes=client_id)
 
-    globals.CONNECTIONS[client_id] = {
-        "address": address,
-        "port": port,
-    }
+    globals.CONNECTIONS[client_id] = ConnectionState(sock, address, port, client_id)
     logger.success(f"Connection established with client {client_id}!")
 
-    Sender(sock, address, port, client_id).start()
-    Reciever(sock, address, port, client_id).start()
+    Sender(globals.CONNECTIONS[client_id]).start()
+    Reciever(globals.CONNECTIONS[client_id]).start()
 
 
 def start(address, port, client_id):
